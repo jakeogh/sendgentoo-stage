2e03760827b59f91aeb3a65996cb0a2127571538
jakeogh <github.com@v6y.net>
2026-07-21 16:48:25 -0700

    update

--- sendgentoo_stage/sendgentoo_stage.py
+++ sendgentoo_stage/sendgentoo_stage.py
@@ -1,27 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-# pylint: disable=useless-suppression             # [I0021]
-# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
-# pylint: disable=missing-param-doc               # [W9015]
-# pylint: disable=missing-module-docstring        # [C0114]
-# pylint: disable=fixme                           # [W0511] todo encouraged
-# pylint: disable=line-too-long                   # [C0301]
-# pylint: disable=too-many-instance-attributes    # [R0902]
-# pylint: disable=too-many-lines                  # [C0302] too many lines in module
-# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive(!)
-# pylint: disable=too-many-return-statements      # [R0911]
-# pylint: disable=too-many-branches               # [R0912]
-# pylint: disable=too-many-statements             # [R0915]
-# pylint: disable=too-many-arguments              # [R0913]
-# pylint: disable=too-many-nested-blocks          # [R1702]
-# pylint: disable=too-many-locals                 # [R0914]
-# pylint: disable=too-many-public-methods         # [R0904]
-# pylint: disable=too-few-public-methods          # [R0903]
-# pylint: disable=no-member                       # [E1101] no member for base
-# pylint: disable=attribute-defined-outside-init  # [W0201]
-# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
-# pylint: disable=C0413  # TEMP isort issue [wrong-import-position] Import "from pathlib import Path" should be placed at the top of the module [C0413]
 
 from __future__ import annotations
 
@@ -170,7 +149,12 @@ def extract_stage3(
     assert isinstance(arch, str)
     assert len(arch) > 0
     destination = Path(destination).resolve()
-    icp(stdlib, arch, destination, vm)
+    icp(
+        stdlib,
+        arch,
+        destination,
+        vm,
+    )
     icp(destination)
     if expect_mounted_destination:
         assert path_is_mounted(
